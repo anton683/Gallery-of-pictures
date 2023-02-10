6406db40b37226e139d3c0bcb23dee6af999e038
Anton <antonio37rus@gmail.com>
2023-02-10 20:15:52 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,19 @@
+// в строковом формате получаем каждый из слайдов
+const slides = document.querySelectorAll(".slide");
+
+// 
+for (const slide of slides) {
+    slide.addEventListener("click", () => { 
+
+        clearActiveClasses()
+
+        slide.classList.add("active")
+
+    })
+}
+
+function clearActiveClasses() {
+    slides.forEach((slide) => {
+        slide.classList.remove("active")
+    })
+}
